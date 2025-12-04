8c58e395cf3ba9f708ec22156ac5f98ea27e2f78
erladion <johan.jansson94@gmail.com>
2025-12-04 13:44:23 +0100

    Remove unnecessary comments

--- server/async_structs.h
+++ server/async_structs.h
@@ -1,14 +1,13 @@
 #ifndef ASYNC_STRUCTS_H
 #define ASYNC_STRUCTS_H
 
-class CallData;  // Forward declaration
+class CallData;
 
 enum OpType { CONNECT, READ, WRITE };
 
-// The Tag wraps the pointer and the operation type.
 struct Tag {
-  CallData* connection;  // Which client is this?
-  OpType type;           // What just finished?
+  CallData* connection;
+  OpType type;
 };
 
 #endif  // ASYNC_STRUCTS_H
